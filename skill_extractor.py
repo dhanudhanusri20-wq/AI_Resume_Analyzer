841f5ed757849eb117359d5e013eb5beebff0286
dhanudhanusri20-wq <dhanudhanusri20@gmail.com>
2026-05-11 20:03:31 +0530

    app.py

--- skill_extractor.py
+++ skill_extractor.py
@@ -1,16 +1,35 @@
-import json
-
-def load_skills():
-    with open("dataset/skills.json", "r") as file:
-        skills = json.load(file)
-    return skills
-
 def extract_skills(text):
-    skills_list = load_skills()
+
+    skills_list = [
+        "python",
+        "java",
+        "c",
+        "c++",
+        "html",
+        "css",
+        "javascript",
+        "sql",
+        "machine learning",
+        "data science",
+        "react",
+        "node js",
+        "django",
+        "flask",
+        "power bi",
+        "excel",
+        "communication",
+        "leadership",
+        "teamwork"
+    ]
+
     found_skills = []
 
+    text = text.lower()
+
     for skill in skills_list:
-        if skill in text:
+
+        if skill.lower() in text:
+
             found_skills.append(skill)
 
-    return list(set(found_skills))
+    return found_skills
